e0769830a886a40227e45f4b2f1befa33444d51c
Mahoromeza50 <mahoromezacarine@gmail>
2019-05-09 03:08:38 -0700

    first commit

---
+++ app/src/main/java/com/example/p217064280/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.p217064280;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username,password;
+    Button register,login;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+        register=(Button)findViewById(R.id.register);
+        login=(Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Login succefull" , Toast.LENGTH_LONG).show();
+                Intent intu = new Intent(MainActivity.this,Login.class);
+                startActivity(intu);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                if (user.equals("") || pass.equals("")){
+                    Toast.makeText(getApplicationContext(), "Fill all field" , Toast.LENGTH_LONG).show();
+
+                }
+                else if (user.equals("carine") && pass.equals("dance1995@")){
+                    Toast.makeText(getApplicationContext(), "Register yourself" , Toast.LENGTH_LONG).show();
+                    Intent inti = new Intent(MainActivity.this,Register.class);
+                    startActivity(inti);
+
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "invalid username or password" , Toast.LENGTH_LONG).show() ;
+            }
+        });
+    }
+}
+
